8c8f1c7c00983ee88d48791f9cd5bc465529579e
Hai Nam <ninhhainam12@gmail.com>
2025-07-01 19:58:36 +0700

    chore: add .env.example

---
+++ src/main/java/com/hainam/judgeql/config/EnvironmentConfig.java
@@ -0,0 +1,12 @@
+package com.hainam.judgeql.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for environment variables
+ * Note: The actual loading of environment variables is done in the main application class
+ */
+@Configuration
+public class EnvironmentConfig {
+    // Environment configuration is handled by DotenvLoader in the main application
+}
